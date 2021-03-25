6ae4ed88173240eff665ef4fe2a684df3fde851f
Lrd134 <larryc3200@gmail.com>
2021-03-25 18:11:13 -0400

    commit tree 807c45ea82441fc2f28ba91cd83ebc6917344c84

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+require './lib/user.rb'
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(known)
+        @knowledge << known
+    end
+    def knowledge
+        @knowledge
+    end
 end
